5afbab2f1de9bb45c617f76df13bf06936317f88
DESKTOP-5GF53UO\ADMIN <luiscolocho1402@gmail.com>
2025-04-26 17:33:54 -0600

    PRIMER TRABAJO EN CLASES "PROGRAMACION WEB"

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App(){
+    return (
+        <div>
+        <h1>Hola Mundo desde React</h1>
+        <p> Creaando mi primera aplicacion desde React</p>
+        </div>   
+    );
+}
